6e0c562251d9fbbe4b415c418140f1dc1c9162bf
Tur Jean-Baptiste <jean-baptiste.tur@student.junia.com>
2025-03-18 11:16:05 +0100

    Partie 1 : Mise en place

---
+++ Model/pdo.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "root"; 
+$password = "root"; 
+$dbname = "junia.sql";
+
+try {
+    $dbPDO = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    
+    $dbPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connexion réussie à la base de données"; 
+} catch(PDOException $e) {
+    echo "La connexion a échouée : " . $e->getMessage();
+}
+?>
